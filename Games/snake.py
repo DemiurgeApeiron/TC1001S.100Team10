76630180058d0252c7a7b9850ebc9bd933c6c29b
Alejandro Javier <40289632+DemiurgeApeiron@users.noreply.github.com>
2021-03-18 11:19:57 -0600

    snake food run(the snake runs for it's food

--- Games/snake.py
+++ Games/snake.py
@@ -13,33 +13,48 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
+limitex = 420
+limitey = 420
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+
 
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+
 def inside(head):
     "Return True if head inside boundaries."
-    return -200 < head.x < 190 and -200 < head.y < 190
+    return -limitex / 2 + 10 < head.x < limitex / 2 - 10 and -limitey / 2 + 10 < head.y < limitey / 2 - 10
+
 
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
+    if food.x < limitex / 2 - 10 and food.x > -limitex / 2 + 10:
+        print(food.x)
+        food.x += round(randrange(-1, 2)) * 10
+    else:
+        food.x += food.x / food.x * -10
+    if food.y < limitey / 2 - 10 and food.y > -limitey / 2 + 10:
+        print(food.y)
+        food.y += round(randrange(-1, 2)) * 10
+    else:
+        food.y += food.y / food.y * -10
 
     if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
+        square(head.x, head.y, 9, "red")
         update()
         return
 
     snake.append(head)
 
     if head == food:
-        print('Snake:', len(snake))
+        print("Snake:", len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
@@ -48,19 +63,21 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, "black")
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, "green")
     update()
     ontimer(move, 100)
+    print(f"snake {head.x} :: {head.y}")
+
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+onkey(lambda: change(10, 0), "Right")
+onkey(lambda: change(-10, 0), "Left")
+onkey(lambda: change(0, 10), "Up")
+onkey(lambda: change(0, -10), "Down")
 move()
 done()
