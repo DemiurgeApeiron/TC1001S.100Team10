5cc1de28efb6d5e78f7097c8acceb0bba0dd353a
Alejandro Javier <40289632+DemiurgeApeiron@users.noreply.github.com>
2021-03-18 16:33:25 -0600

    Update pacman.py
    added comments

--- pacman.py
+++ pacman.py
@@ -22,6 +22,7 @@ ghosts = [
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
+    #added 2 more gost
     [vector(-50, -80), vector(-5, 0)],
     [vector(50, 80), vector(-5, 0)],
 ]
